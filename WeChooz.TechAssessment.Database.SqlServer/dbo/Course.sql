3417132d16eef60dd31c62cd6697067af0966853
bourams <brahim.benmansour@outlook.fr>
2025-10-28 08:10:11 +0100

    add core training tables (Course, Session, Participant)

---
+++ WeChooz.TechAssessment.Database.SqlServer/dbo/Course.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Course] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(200) NOT NULL,
+    [ShortDescription] NVARCHAR(500) NULL,
+    [LongDescriptionMarkdown] NVARCHAR(MAX) NULL,
+    [DurationDays] INT NOT NULL,
+    [TargetAudience] NVARCHAR(50) NOT NULL, -- 'Elu' or 'President'
+    [Capacity] INT NOT NULL,
+    [Trainer] NVARCHAR(150) NOT NULL,
+    [CreatedAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
+);
---
+++ WeChooz.TechAssessment.Database.SqlServer/dbo/Participant.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Participant] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [SessionId] INT NOT NULL,
+    [FirstName] NVARCHAR(100) NOT NULL,
+    [LastName] NVARCHAR(100) NOT NULL,
+    [Email] NVARCHAR(255) NOT NULL,
+    [CompanyName] NVARCHAR(255) NULL,
+    [RegisteredAt] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+    CONSTRAINT FK_Participant_Session FOREIGN KEY (SessionId)
+        REFERENCES [dbo].[Session](Id)
+        ON DELETE CASCADE
+);
