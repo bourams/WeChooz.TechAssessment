2d06d0efa549a0b1daf3d39e500895ae526b0b4e
bourams <brahim.benmansour@outlook.fr>
2025-10-29 12:49:47 +0100

    refactor(web): remove direct database and cache dependencies from web

--- WeChooz.TechAssessment.Web/Program.cs
+++ WeChooz.TechAssessment.Web/Program.cs
@@ -7,8 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
-var sqlServerConnectionString = builder.Configuration.GetConnectionString("formation") ?? throw new InvalidOperationException("Connection string 'formation' not found.");
-var redisConnectionString = builder.Configuration.GetConnectionString("cache") ?? throw new InvalidOperationException("Connection string 'cache' not found.");
+//var redisConnectionString = builder.Configuration.GetConnectionString("cache") ?? throw new InvalidOperationException("Connection string 'cache' not found.");
 
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<RazorViewEngineOptions>(options =>
