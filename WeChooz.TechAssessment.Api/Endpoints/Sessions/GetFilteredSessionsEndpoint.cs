48a0eeb23946a317be3f3899098c5a6f9c097c91
bourams <brahim.benmansour@outlook.fr>
2025-10-30 11:08:23 +0100

    feat(api): add service and endpoint to retrieve sessions with filters

---
+++ WeChooz.TechAssessment.Api/Endpoints/Sessions/GetFilteredSessionsEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using WeChooz.TechAssessment.Api.Dtos;
+using WeChooz.TechAssessment.Api.Dtos.Filters;
+using WeChooz.TechAssessment.Api.Services;
+
+namespace WeChooz.TechAssessment.Api.Endpoints.Sessions;
+
+public class GetFilteredSessionsEndpoint : EndpointBaseAsync.WithRequest<SessionFilter>
+                                                            .WithActionResult<IEnumerable<SessionListDto>>
+{
+    private readonly ISessionService _service;
+
+    public GetFilteredSessionsEndpoint(ISessionService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("api/sessions")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<SessionListDto>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public override async Task<ActionResult<IEnumerable<SessionListDto>>> HandleAsync([FromQuery] SessionFilter filter,
+                                                                                      CancellationToken cancellationToken = default)
+    {
+        var result = await _service.GetSessionWithFiltersAsync(filter, cancellationToken);
+
+        return Ok(result);
+    }
+}
---
+++ WeChooz.TechAssessment.Api/Services/SessionService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using WeChooz.TechAssessment.Api.Dtos;
+using WeChooz.TechAssessment.Api.Dtos.Filters;
+using WeChooz.TechAssessment.Api.Persistance.FiltersSpecifications;
+using WeChooz.TechAssessment.Api.Persistance.Repositories;
+
+namespace WeChooz.TechAssessment.Api.Services;
+
+public class SessionService : ISessionService
+{
+    private readonly ISessionRepository _sessionRepository;
+    private readonly IMapper _mapper;
+
+    public SessionService(ISessionRepository sessionRepository, IMapper mapper)
+    {
+        _sessionRepository = sessionRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<SessionListDto>> GetSessionWithFiltersAsync(SessionFilter sessionFilters,
+                                                                              CancellationToken ct = default)
+    {
+        var spec = _mapper.Map<SessionFilterSpec>(sessionFilters);
+
+        //TODO: Add pagination
+        var sessions = await _sessionRepository.ListAsync(spec, ct);
+        
+        return _mapper.Map<IEnumerable<SessionListDto>>(sessions);
+    }
+}
